6fb70eb5e12a6762d97d3a9fcb1d093f3f72ccc2
RUKUNDO Princde <rukundoprince951@gmail.com>
2023-06-07 07:50:12 +0300

    finished the hero section

--- src/components/Demo.jsx
+++ src/components/Demo.jsx
@@ -1,10 +1,17 @@
 import React from 'react'
+import { useState, useEffect } from 'react'
+import { copy, linkIcon, loader, tick } from '../assets'
 
 const Demo = () => {
   return (
-    <div>
-      Demo
-    </div>
+    <section className='mt-16 w-full max-w-xl'>
+      {/* Search */}
+      <div className='flex flex-col w-full gap-2'>
+        <form action="" className='relative flex justify-center items-center' onSubmit={() => {}}>
+          <img src={linkIcon} alt="link_icon" className='absolute left-0 my-2 ml-3 w-5' />
+        </form>
+      </div>
+    </section>
   )
 }
 
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -8,6 +8,13 @@ const Hero = () => {
         <img src={logo} alt="logo" className='w-28 object-contain' />
         <button type='button' onClick={() => window.open('https://github.com/RUKUNDO-Prince/article-summarizer')} className='black_btn'>Github</button>
       </nav>
+      <h1 className='head_text'>
+        Summarize articles with <br className='max-md: hidden' />
+        <span className='orange_gradient'>OpenAI GPT-4</span>
+      </h1>
+      <h2 className='desc'>
+        Simplify your reading with Summize, an open-source article summarizer that transforms lengthy articles into Clear and Concise Summaries. 
+      </h2>
     </header>
   )
 }
